908524326b6926c0d87666aca929e60b8d2eae41
LiJiaWei <525469647@qq.com>
2019-08-11 13:25:57 +0800

    打包

---
+++ build/precache-manifest.1bcd425236221535d85ce8136920da9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d5bf786920e4ca44e83",
+    "url": "/static/js/runtime~main.9d5bf786.js"
+  },
+  {
+    "revision": "1ffa94244c7c0f4c93a2",
+    "url": "/static/js/main.1ffa9424.chunk.js"
+  },
+  {
+    "revision": "f27063c080dd64e98296",
+    "url": "/static/js/LoginPage.f27063c0.chunk.js"
+  },
+  {
+    "revision": "73fbd2797f1a444218ac",
+    "url": "/static/js/BikeMapPage.73fbd279.chunk.js"
+  },
+  {
+    "revision": "6993a46bdecdb1feb46c",
+    "url": "/static/js/3.6993a46b.chunk.js"
+  },
+  {
+    "revision": "6993a46bdecdb1feb46c",
+    "url": "/static/css/3.48844659.chunk.css"
+  },
+  {
+    "revision": "17edf23340a33695874ac423920fab61",
+    "url": "/index.html"
+  }
+];
